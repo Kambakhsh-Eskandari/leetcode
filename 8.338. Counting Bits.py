166893e122af529e03a5fe36259ad615af5b2f00
Kambakhsh Eskandari <kambakhsh.eskandari1@gmail.com>
2023-04-02 19:39:24 +0430

    Solution 8

---
+++ 8.338. Counting Bits.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        ans = [0] * (n+1)
+        off = 1
+
+        for i in range(1,n+1):
+            if off * 2 == i:
+                off = i
+            ans[i] = 1 + ans[i - off]
+
+        return ans
+        
